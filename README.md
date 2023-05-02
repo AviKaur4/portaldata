65749325aa1f2ee82aa35c3ec3b74153b0e2fa65
AviKaur4 <avineetkaurchahal@gmail.com>
2023-05-02 15:35:03 -0700

    Sending these over

---
+++ README.md
@@ -0,0 +1,26 @@
+
+# portaldata
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of portaldata is to ...
+
+## Installation
+
+You can install the development version of portaldata from [GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("AviKaur4/Bio197")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(portaldata)
+## basic example code
+```
+
---
+++ data-raw/DATASET.R
@@ -0,0 +1,16 @@
+## code to prepare `DATASET` dataset goes here
+
+taxo <- read.csv(file = "data-raw/portal-species-taxonomy.csv")
+download.file(url = "portal-species-taxonomy.csv", destfile = "data-raw/portal-species-taxonomy.csv")
+function.portal <- read.csv("data-raw/portal-species-taxonomy.csv")
+
+
+## code to prepare `DATASET` dataset goes here
+
+taxo <- read.csv(file = "data-raw/portal-species-taxonomy.csv")
+usethis::use_data(taxo, overwrite = TRUE)
+head(taxo)
+str(taxo)
+nrow(taxo)
+colnames(taxo)
+document()
